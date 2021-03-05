7cf9671ee00cf6fd78e2272de8df8ea8e97dedfa
Daniel Silva <daniel.raizon@gmail.com>
2021-03-05 03:45:51 -0300

    Use interfaces to type functions, objects and arrays.

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,13 @@
 import { Request, Response } from "express";
 
+import createUser from "./services/CreateUser";
+
 export function helloWorld(request: Request, response: Response) {
+	const user = createUser({
+		email: "daniel@gmail.com",
+		password: "12345",
+		techs: ["Node.js", "ReactJS", "React Native"],
+	});
+
 	return response.json({ message: "Hello World" });
 }
