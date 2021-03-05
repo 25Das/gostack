04d1951227a0619a889293013af6ad0123677075
Daniel Silva <daniel.raizon@gmail.com>
2021-03-05 02:04:15 -0300

    Add funcionality of create and show new projects via mobile.

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -5,6 +5,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableOpacity,
 } from "react-native";
 
 import api from "./services/api";
@@ -19,6 +20,17 @@ export default function App() {
       setProjects(response.data);
     });
   }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Daniel Alves"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
   
   return (
     <>
@@ -32,6 +44,14 @@ export default function App() {
           <Text style={styles.project}>{project.title}</Text>
         )}
       />
+
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -42,8 +62,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#7159c1",
   },
+
   project: {
     color: "#FFF",
     fontSize: 30,
+  },
+
+  button: {
+    backgroundColor:"#FFF",
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
